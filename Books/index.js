baddc608d9b64a2491eda97efe39f17e21abaa07
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-10-18 17:44:45 +0100

    Uncomment deploy step  & Change to test the book service

--- Books/index.js
+++ Books/index.js
@@ -21,7 +21,7 @@ var apiURL = "https://www.abibliadigital.com.br/api/books"
 
 
 app.get('/', (req, res) => {
-    res.send('Hello, world <---> Books Microservice 7080!');
+    res.send('Hello, world <---> Books Microservice 7071!');
 });
 
 // Fetch all books from the external API
