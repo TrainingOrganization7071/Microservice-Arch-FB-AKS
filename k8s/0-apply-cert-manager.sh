78501f1bb20f3df0a33af201ede6aca71e3d85bb
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-10-24 17:54:20 +0100

    Change : Just HTTP

--- k8s/0-apply-cert-manager.sh
+++ k8s/0-apply-cert-manager.sh
@@ -6,7 +6,7 @@ kubectl get all --namespace cert-manager
 
 echo '\n ################################### \n ################################### \n'
 
-kubectl apply -f 1-nginx-internal-controller.yml
+kubectl apply -f 1-nginx-internal-controller.yaml
 
 sleep 30
 
@@ -18,25 +18,26 @@ echo '\n #########-- Record the Nginx internal controller in the DNS --#########
 
 echo '\n Waitting ... \n'
 
-read 
+#read 
+#
+#echo '\n ################################### \n ################################### \n'
+#
+#kubectl apply -f 2-cluster-issuer.yaml
+#
+#sleep 15
+#
+#kubectl get clusterissuer -owide
+#
+#echo '\n ################################### \n ################################### \n'
+#
+#kubectl apply -f 3-ingress-internal.yaml
+#
+#sleep 30
+#
+#kubectl get ingress -owide  
+#
+#echo '\n ################################### \n ################################### \n'
 
-echo '\n ################################### \n ################################### \n'
-
-kubectl apply -f 2-cluster-issuer.yaml
-
-sleep 15
-
-kubectl get clusterissuer -owide
-
-echo '\n ################################### \n ################################### \n'
-
-kubectl apply -f 3-ingress-internal.yaml
-
-sleep 30
-
-kubectl get ingress -owide  
-
-echo '\n ################################### \n ################################### \n'
 
 
 
