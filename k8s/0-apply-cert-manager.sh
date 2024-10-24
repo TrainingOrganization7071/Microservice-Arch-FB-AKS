7c8d75885cc17975f2c12bc1dde46df8f9e06200
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-10-24 16:38:30 +0100

    Change manifist files to use https

---
+++ k8s/0-apply-cert-manager.sh
@@ -0,0 +1,43 @@
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.13.0/cert-manager.yaml
+
+sleep 10
+
+kubectl get all --namespace cert-manager
+
+echo '\n ################################### \n ################################### \n'
+
+kubectl apply -f 1-nginx-internal-controller.yml
+
+sleep 30
+
+kubectl get nginxingresscontroller
+
+echo '\n ################################### \n ################################### \n'
+
+echo '\n #########-- Record the Nginx internal controller in the DNS --############ \n'
+
+echo '\n Waitting ... \n'
+
+read 
+
+echo '\n ################################### \n ################################### \n'
+
+kubectl apply -f 2-cluster-issuer.yaml
+
+sleep 15
+
+kubectl get clusterissuer -owide
+
+echo '\n ################################### \n ################################### \n'
+
+kubectl apply -f 3-ingress-internal.yaml
+
+sleep 30
+
+kubectl get ingress -owide  
+
+echo '\n ################################### \n ################################### \n'
+
+
+
+
